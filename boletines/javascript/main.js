8ccfa5db745fe82b89dcfa8be36fe6e61e3b76cc
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2020-06-16 04:27:31 -0500

    Corrigiendo

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -20,7 +20,7 @@ links.forEach(function(link){
            content.classList.add("animate__animated");
 
             setTimeout(function(){
-                location.href= "/boletines";
+                location.href= "/piztux.github.com/boletines/";
             },600);
            
 
