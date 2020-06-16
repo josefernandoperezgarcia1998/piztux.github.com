e3326a0f208ef3b57e49f4473780f035b72f4866
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2020-06-16 04:20:30 -0500

    Subiendo por primera vez bien el proyecto de pizzas.

---
+++ boletines/javascript/main.js
@@ -0,0 +1,30 @@
+//Obtener los elementos de la clase close
+
+let links = document.querySelectorAll(".close");
+
+//Recorrerlos
+
+links.forEach(function(link){
+
+        //Agregar un evento click a cada elemento
+        link.addEventListener("click",function(ev){
+           ev.preventDefault();     
+            let content = document.querySelector('.content');
+           
+            //Quitarle las clases de animación
+            content.classList.remove("animate__fadeInDown");
+            content.classList.remove("animate__animated");
+
+           //Agregar clases para animar su salida
+           content.classList.add("animate__fadeOutUp");
+           content.classList.add("animate__animated");
+
+            setTimeout(function(){
+                location.href= "/boletines";
+            },600);
+           
+
+           return false;
+        });
+});
+
